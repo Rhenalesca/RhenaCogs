60c69294719015e1ef29625040dc0cbe71d5d94d
Rhenalesca <31389553+Rhenalesca@users.noreply.github.com>
2017-10-03 02:02:55 -0600

    Update guessnumber.py

--- guessnumber.py
+++ guessnumber.py
@@ -1,1 +1,18 @@
-tbd
+import discord
+from discord.ext import commands
+
+class Mycog:
+    """My custom cog that does stuff!"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+@commands.command()
+async def kick(self, user : discord.Member):
+    """I will kick anyone! >.<"""
+await self.bot.say("ONE PUNCH! And " + user.mention + " is out! ლ(ಠ益ಠლ)")
+
+
+def setup(bot):
+    bot.add_cog(Mycog(bot))
+    
